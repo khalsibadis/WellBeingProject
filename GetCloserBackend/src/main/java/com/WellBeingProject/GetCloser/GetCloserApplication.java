40d977b2e46a3b6c0a04a24f25ad89aaca6b0240
aymen-jbara <aymen.jb.06@gmail.com>
2022-02-22 20:10:00 +0100

    Projet Spring

---
+++ GetCloserBackend/src/main/java/com/WellBeingProject/GetCloser/GetCloserApplication.java
@@ -0,0 +1,13 @@
+package com.WellBeingProject.GetCloser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GetCloserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GetCloserApplication.class, args);
+	}
+
+}
---
+++ GetCloserBackend/src/test/java/com/WellBeingProject/GetCloser/GetCloserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.WellBeingProject.GetCloser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GetCloserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
